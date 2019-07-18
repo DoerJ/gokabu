be37e054cdbedd8a75c8e4a23f54bc2bbe1b77ae
jamesHe <jamesyuhao@hotmail.com>
2019-07-18 14:12:57 -0700

    default-list added

--- gokabu/src/Components/widgets/store-card.js
+++ gokabu/src/Components/widgets/store-card.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import '../../Styles/widgets/store-card.css';
 
 const StoreCard = (props) => {
     return (
         <div className="store-card-wrapper">
-            <div className="store-name-block">{props.name}</div>
+            <div className="store-image-block"><img src={props.image}/></div>
+            <div className="store-info-block">
+                <div className="store-name-block">{props.name}</div>
+                <div className="store-distance-block">{props.distance} km</div>
+                <div className="store-rating-block">Rating: {
+                    (!props.rating ? 5 : props.rating)
+                }</div>
+            </div>
         </div>
     );
 }
